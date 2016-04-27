116b82f53fffae34b064581ef2ef504abe4a3826
Fernando Concatto <fernandoconcatto@gmail.com>
2016-04-27 17:59:22 -0300

    Interação com socket adicionado

--- index.js
+++ index.js
@@ -4,14 +4,24 @@ var app = express();
 var server = http.Server(app);
 var io = require("socket.io")(server);
 
+app.set("views", "./views");
+app.set("view engine", "pug");
+
 app.use(express.static("public"));
 
 app.get("/", function(req, res) {
-    res.sendFile(__dirname + "/index.html");
+    res.render("index");
 });
 
-io.on("connection", function(socket) {
+var lifeValues = [20, 20];
 
+io.on("connection", function(socket) {
+    socket.on("action", function(data) {
+        var increment = data.command == "increase" ? 1 : -1;
+        lifeValues[parseInt(data.player)] += increment;
+
+        io.emit("lifeChange", {values: lifeValues});
+    });
 });
 
 server.listen(3000);
